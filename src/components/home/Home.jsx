e2c5c8514d9e65aba93452bd6529cf95fed8222b
Elibako <elibako98@gmail.com>
2024-10-18 22:52:53 +0100

    completed sectionn 6

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -9,6 +9,8 @@ const Home = () => {
         {/* fatima home section3 */}
         {/* aduraleke home section4 */}
         {/* omolola home section5 */}
+        {/*Elisha home section6*/}
+        <p>Home Section 6</p>         
     </div>
   )
 }
